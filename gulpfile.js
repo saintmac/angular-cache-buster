81ac7e47c237d86a94fd48bfc55cb72fa4873dc1
Jerke Combee <j.combee@ultraware.nl>
2016-05-26 15:34:28 +0200

    Added jshint and fixed jshint warnings

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var minify = require('gulp-minify');
 
 gulp.task('compress', function () {
     gulp.src('lib/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
         .pipe(minify({
             ext: {
                 src: '.js',
